afc23007bdc2ed6421216648b2d6535cc86466b6
PC-w10-22 <PC-w10-22@192.168.1.113>
2016-12-23 15:01:27 +0100

    Récupération fichiers

--- app.js
+++ app.js
@@ -1,61 +1,96 @@
-var express = require('express');
-var app = express();
+///////////////////////////////////////////////////----->require
+var xpress = require('express');
 var passport = require('passport');
+//bodyparser va recuperer les element de l'url pour recuperer les donner du formulaire
 var bodyparser = require('body-parser');
 var session = require('express-session');
-var authentification = require('authentification');
-var dateformat = require('dateformat');
+var app = xpress();
+//on appel le module que l'on a créé dnas node_module'
+var auth = require('authentification');
+//on appel le module qui s'ocupe des routes
 var routing = require('routing');
+//on appel le module qui s'ocupe du caddie
 var caddierouting = require('caddierouting');
-var df = require('dateformat');
-var cfg = require('config');
 
+var url = require('url');
 //on appel le module qui s'occupe des routes admin
 var admin = require('admin');
+//on appel le module qui s'occupe de config
+var cfg = require('config');
+//on appel le module qui s'occupe d'envoyer des mails
+var mailrouting = require('emailrouting');
+//on appel le module product routing correstion du product manager
+var productrouting = require('productrouting');
+//on appel le module order routing correction du order manager
+var oderrouting = require('oderrouting');
+//on appel le module order routing correction du order manager
+var stockoderrouting = require('stockoderrouting');
 
-//permet d'utiliser les CSS et images
-app.use(express.static('public'));
-
+///////////////////////////////////////////////////----->USE
+//pour utiliser les fichiers static css img js
+app.use(xpress.static('public'));
 //pour utiliser les fichiers static en admin
-app.use(express.static('public/admin'));
-
-//passport utilise bodyparser pour récupérer le contenu des formulaire sous forme de tableau json
+app.use(xpress.static('public/admin'));
+//configuration du module passport
+//pour que passport puisse recuperer sous forme de tablo en json le rersultat du formulaire
 app.use(bodyparser.json());
-
-//cela signifie que bodyparser accepte utf8
+//bodyparser accepte utf8
 app.use(bodyparser.urlencoded({extended: true}));
-
-//permet de crypter les données stockées en session
-app.use(session({secret:"Thisissecret", resave:true, saveUninitialized:true}));
-
-//initialise passport tel que configuré
+//c'est le mot secret pour pouvoir crypter et la persistance dans le serveur avec initialized
+app.use(session({secret:"Thissecret",resave:true, saveUminitialized : true}));
+// initialise passport tel que confirer
 app.use(passport.initialize());
-
-//démarre passport en mode session
+//et demare passport en monde session
 app.use(passport.session());
+//Utilisation du product routing de fabrice
+app.use('/admin',productrouting);
+//Utilisation du order routing de fabrice
+app.use('/admin',oderrouting);
+//Utilisation du order routing de fabrice
+app.use('/admin',stockoderrouting);
 
-app.use(function(req,resp,next){
-		var user = 'anonymous';
-		if(typeof req.user!='undefined')
-			user = req.user.username
-			console.log('Time:',dateformat(Date.now(),cfg.APP.dateformat),
-					req.method,user,req.url);
-			next();						
+//pour utiliser un format de date lisible
+var df = require('dateformat');
+
+app.use(function(req,res,next){
+    var user = 'anonymous'
+    if(typeof req == 'undefined'){
+        console.log('Time : ',df(Date.now(),cfg.APP.dateformat),req.method,user,req.url);
+        }
+    //le next est la pour appliquer tout ce qui est avant à le faire à tout ce qui suit
+    next();
 });
+
+
+
+//pour voir dans le log la route , get ou post et la date
+// On utilise routing.js dans le node_module, bien le mettre à la fin, car tout est configuré pour faire marché ce qui suit
+app.use("/",routing);
+
 
 //on utilise la route admin comme redirection avec le fichier admin
 app.use("/admin",admin);
 
-app.use("/",routing);
-
-//app.use("/",caddierouting);
+//on utilise la route / comme redirection avec le fichier caddirouting
 app.use("/",caddierouting);
 
-//gestion de la page 404
-app.use(function (req,resp){
-		if(typeof req.user=='undefined'){
-			resp.status(404).render('404.html.twig');}
-			else {resp.status(404).render('404.html.twig',{"username":req.user.username})};						
-});
+//on utilise le mailrouting pour l'envoi de mail
+app.use("/",mailrouting);
 
+//gestion de la page 404
+app.use(function(req,resp){
+    //si on eu une requent qui n'a eu aucune reponse
+    if(typeof req.user =='undefined'){
+        //alors on redirige vers la page 404
+        resp.status(404).render('404.html.twig');
+
+    }
+    else
+    {
+        resp.status(404).render('404.html.twig',{"username":req.user.username});
+    }
+resp.render
+
+});
+//on ecoute l'aplication
 app.listen(8088);
--- mysql.js
+++ mysql.js
@@ -1,28 +1,43 @@
-var mysql=require('mysql');
-var xpress=require('express');
-var app=xpress();
+var mysql = require ('mysql');
+var express = require('express');
+var app = express();
 
-app.get("/",function(reqa,resp){
-	var connection=mysql.createConnection({
-		host:"localhost",
-		database:"formation",
-		user:"poweruser",
-		password:"power",
-	})
-	
-	connection.connect();
-	connection.query('select * from user',
-			function(err,rows,fields){
-		if(!err){
-			if(rows.length>0){
-				for(var i=0;i<rows.length;i++){
-					console.log(rows[i].username);
-				}
-			}
-		}
-		else console.log(err);
-	});
-	 connection.end();
+app.get("/", function(req,resp){
+    //config pour la connection
+    var connection = mysql.createConnection({
+        host :"localhost",
+        database :"site_e_commerce",
+        user :"superoot",
+        pasword:"route2route"
+    });
+    //connection avec la bdd
+    connection.connect();
+
+
+    //executer la query puis faire ujne fonction de calback
+    connection.query('SELECT * FROM `user` ', function(err,rows,fields)
+    {
+        //si je n'ai pas d'erreur 
+        if(!err)
+        {
+            //et que mon resultat est plus grand que zero
+            if(row.length>0)
+            {
+                //il va rechercher tous les user et dans la console on va lui construire le username
+                for (var i=0; i<rows.length;i++)
+                    {
+                        dbuser = rows[i].username;
+                    }
+            }
+
+        }
+        else
+        console.log(err);
+
+    });
+
+    connection.end();
 });
 
 app.listen(8088);
+
